b5a8ccf601cc126879304df7307f768e689e13e2
de Witte <Benoit.DeWitte@student.uliege.be>
2021-02-26 12:03:55 +0100

    Corrections and add insert method

--- arrays/DynamicArray.py
+++ arrays/DynamicArray.py
@@ -29,12 +29,21 @@ class DynamicArray:
         """Add object to the end of the array."""
         if self._n == self._capacity:
             self._resize(2 * self._capacity)
-        self._A.append(obj)
+        self._A[self._n] = obj
+        self._n += 1
+
+    def insert(self, k, value):
+        """Insert value at index k, shifting subsequent values rightward."""
+        if self._n == self._capacity:
+            self._resize(2 * self._capacity)
+        for j in range(self._n, k, -1):
+            self._A[j] = self._A[j - 1]
+        self._A[k] = value
         self._n += 1
 
     def _resize(self, c):
         """Resize internal array to capacity c."""
-        B = self._make_array(self, c)
+        B = self._make_array(c)
         for i in range(self._n):
             B[i] = self._A[i]
         self._A = B
@@ -43,3 +52,14 @@ class DynamicArray:
     def _make_array(self, c):
         """ Return new array with capacity c."""
         return (c * ctypes.py_object)()
+
+    def __repr__(self):
+        return str(self._A[: self._n])
+
+
+if __name__ == "__main__":
+    test = DynamicArray()
+    test.append(1)
+    test.append(2)
+    test.insert(1, 5)
+    print(test)
